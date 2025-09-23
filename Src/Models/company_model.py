7a65238397d76283e0dee0d0f9e6e21a90a6be39
Kirilligu <k.degtyarev4@mail.ru>
2025-09-23 22:12:31 +0800

    Исправил Settings и company_model

--- Src/Models/company_model.py
+++ Src/Models/company_model.py
@@ -2,8 +2,12 @@
 ###############################################
 # Модель организации
 class company_model:
-    __name:str = ""
-    __inn:str = ""
+    __name: str = ""
+    __inn: str = ""
+    __account: str = ""
+    __corr_account: str = ""
+    __bik: str = ""
+    __ownership: str = ""
 
     # Наименование
     @property
@@ -11,8 +15,62 @@ class company_model:
         return self.__name
 
     @name.setter
-    def name(self, value:str):
+    def name(self, value: str):
         if value.strip() != "":
             self.__name = value.strip()
+        else:
+            raise Exception("Наименование не может быть пустым")
 
+    #инн
+    @property
+    def inn(self) -> str:
+        return self.__inn
+    @inn.setter
+    def inn(self, value: str):
+        if len(value.strip()) == 12:
+            self.__inn = value.strip()
+        else:
+            raise Exception("ИНН должен быть 12 символов")
+    #счет
+    @property
+    def account(self) -> str:
+        return self.__account
+    @account.setter
+    def account(self, value: str):
+        if len(value.strip()) == 11:
+            self.__account = value.strip()
+        else:
+            raise Exception("Счёт должен быть 11 символов")
 
+    #корр счёт
+    @property
+    def corr_account(self) -> str:
+        return self.__corr_account
+    @corr_account.setter
+    def corr_account(self, value: str):
+        if len(value.strip()) == 11:
+            self.__corr_account = value.strip()
+        else:
+            raise Exception("Корреспондентский счёт должен быть 11 символов")
+
+    #бик
+    @property
+    def bik(self) -> str:
+        return self.__bik
+    @bik.setter
+    def bik(self, value: str):
+        if len(value.strip()) == 9:
+            self.__bik = value.strip()
+        else:
+            raise Exception("БИК должен быть 9 символов")
+
+    #вид собственности
+    @property
+    def ownership(self) -> str:
+        return self.__ownership
+    @ownership.setter
+    def ownership(self, value: str):
+        if len(value.strip()) == 5:
+            self.__ownership = value.strip()
+        else:
+            raise Exception("Вид собственности должен быть 5 символов")
